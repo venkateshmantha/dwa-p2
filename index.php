a8aa538c195b536cb7a1504da02c746874cc900d
venkateshmantha <manthavenkatesh@gmail.com>
2018-09-28 23:28:51 -0400

    changes

--- index.php
+++ index.php
@@ -10,28 +10,69 @@
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
           crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
     <title>Unit Converter</title>
 </head>
 
 <body>
 <div class="container">
-    <div class="row">
-        <div class="col"></div>
-        <div class="col">
-            <h1>Unit Converter App</h1>
+    <div class="row mt-5">
+        <h1 class="col-centered">Unit Converter</h1>
+    </div>
+    <div class="row mt-2">
+        <div class="col-xs-4 col-centered">
             <div class="card">
-                <div class="card-header">
-                    Select the Unit of conversion
-                </div>
                 <div class="card-body">
-                    <h5 class="card-title">Dropdown placeholder</h5>
-                    <p class="card-text">Input placeholder</p>
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#length">Length</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#temperature">Temperature</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#area">Area</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#weight">Weight</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#time">Time</a>
+                        </li>
+                    </ul>
+
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                        <div id="length" class="container tab-pane active"><br>
+                            <h3>Length</h3>
+                            <p>Lorem ipsum sectetur adipisicing elitusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        <div id="temperature" class="container tab-pane fade"><br>
+                            <h3>Temperature</h3>
+                            <p>Ut enim ad minim exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div id="area" class="container tab-pane fade"><br>
+                            <h3>Area</h3>
+                            <p>Sed ut perspiciatis unde error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                        </div>
+                        <div id="weight" class="container tab-pane fade"><br>
+                            <h3>Weight</h3>
+                            <p>Ut enim ad minim exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div id="time" class="container tab-pane fade"><br>
+                            <h3>Time</h3>
+                            <p>Sed ut perspiciatis unde error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                        </div>
+                    </div>
                     <a href="#" class="btn btn-primary">Convert</a>
                 </div>
             </div>
         </div>
-        <div class="col"></div>
     </div>
 </div>
 </body>
